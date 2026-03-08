c5c46747d78c955fbe0a42df17581a4493294a0c
Gupta <akg008@ieng6-ece-03.ucsd.edu>
2026-03-07 16:26:30 -0800

    multiplier_pipelined

--- target/target_defines.v
+++ target/target_defines.v
@@ -1,7 +1,8 @@
-//`define CLK_GATE
+`define CLK_GATE
 `define STEP_1
 `define STEP_2
 `define STEP_4
 `define DUAL_CORE_EN
 `define RAND_SEED 50
-//`define RANDOM_TEST_MODE 
+//`define RANDOM_TEST_MODE
+//`define MULT_PIPE_EN 
